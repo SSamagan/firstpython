2607ee8f1de2aef15f83733d90dd751ed793e8ba
SSamagan <0772259532@mail.ru>
2021-09-24 15:30:49 +0600

    python telegram bot

---
+++ pars-news-kg.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup as BS
+import lxml
+import json
+link = "https://iro.oshsu.kg/pages/news_all/"
+headers={
+    ""
+}
+req = requests.get(url=link, headers=headers, verify=False)
+soup = BS(req.content, "lxml")
+title = soup.find_all('a', class_ = 'image-post')
+for el in title:
+
+    print(el.text)
